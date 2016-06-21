fbfe164c9092fef7ab86187b5bbd744dfa5cfd74
lstanard <louis.stanard@gmail.com>
2016-06-21 13:26:46 -0600

    Quick backup commit

--- public/js/main.entry.js
+++ public/js/main.entry.js
@@ -50,17 +50,43 @@
 
 	var favlistApp = angular.module('favlist', ['ngResource']);
 
+	// Services
 	favlistApp
 		.factory('FavListService', function ($resource) {
-			return $resource('http://localhost:3000/lists/:id', {id: '@id'});
+			return $resource(
+				'http://localhost:3000/lists/:listId',
+				{ listId: '@listId' }
+			);
+		})
+		.factory('FavListItemsService', function ($resource) {
+			return $resource(
+				'http://localhost:3000/lists/:listId/list-items/:listItemId',
+				{ listId: '@listId', listItemId: '@listItemId' }
+			);
 		});
 
+	// Filters
+	// favlistApp
+	// 	.filter('ratings', function () {
+	// 		return function () {
+
+	// 		}
+	// 	});
+
+	// Controllers
 	favlistApp
-		.controller('FavListController', function ($scope, $http, FavListService) {
+		.controller('FavListController', function ($scope, $http, FavListService, FavListItemsService) {
 			var favlist = this;
 
 			// Retrieve lists
-			favlist.lists = FavListService.query();
+			favlist.lists = FavListService.query(function () {
+				// Retrieve list items
+				favlist.lists.forEach(function (list) {
+					list.listItems = FavListItemsService.query(
+						{ listId: list.id }
+					);
+				});
+			});
 
 			// Add list action
 			favlist.addList = function () {
@@ -76,15 +102,26 @@
 			favlist.removeList = function (index) {
 				var listId = favlist.lists[index].id;
 				FavListService.delete(
-					{ id: listId },
+					{ listId: listId },
 					function () {
 						favlist.lists.splice(index, 1);
 					}
 				);
 			}
-		})
-		.controller('FavListItemsController', function ($scope) {
-			var favlistItems = this;
+
+			// Remove list item action
+			favlist.removeListItem = function (listIndex, listItemIndex) {
+				var listId = favlist.lists[listIndex].id;
+				var listItemId = favlist.lists[listIndex].listItems[listItemIndex].id;
+
+				FavListItemsService.delete(
+					{ listId: listId, listItemId: listItemId },
+					function () {
+						// Remove from local $scope
+						favlist.lists[listIndex].listItems.splice(listItemIndex, 1);
+					}
+				);
+			}
 		});
 
 /***/ },
--- public/main.js
+++ public/main.js
@@ -4,17 +4,43 @@ var ngResource = require('angular-resource');
 
 var favlistApp = angular.module('favlist', ['ngResource']);
 
+// Services
 favlistApp
 	.factory('FavListService', function ($resource) {
-		return $resource('http://localhost:3000/lists/:id', {id: '@id'});
+		return $resource(
+			'http://localhost:3000/lists/:listId',
+			{ listId: '@listId' }
+		);
+	})
+	.factory('FavListItemsService', function ($resource) {
+		return $resource(
+			'http://localhost:3000/lists/:listId/list-items/:listItemId',
+			{ listId: '@listId', listItemId: '@listItemId' }
+		);
 	});
 
+// Filters
+// favlistApp
+// 	.filter('ratings', function () {
+// 		return function () {
+
+// 		}
+// 	});
+
+// Controllers
 favlistApp
-	.controller('FavListController', function ($scope, $http, FavListService) {
+	.controller('FavListController', function ($scope, $http, FavListService, FavListItemsService) {
 		var favlist = this;
 
 		// Retrieve lists
-		favlist.lists = FavListService.query();
+		favlist.lists = FavListService.query(function () {
+			// Retrieve list items
+			favlist.lists.forEach(function (list) {
+				list.listItems = FavListItemsService.query(
+					{ listId: list.id }
+				);
+			});
+		});
 
 		// Add list action
 		favlist.addList = function () {
@@ -30,13 +56,24 @@ favlistApp
 		favlist.removeList = function (index) {
 			var listId = favlist.lists[index].id;
 			FavListService.delete(
-				{ id: listId },
+				{ listId: listId },
 				function () {
 					favlist.lists.splice(index, 1);
 				}
 			);
 		}
-	})
-	.controller('FavListItemsController', function ($scope) {
-		var favlistItems = this;
+
+		// Remove list item action
+		favlist.removeListItem = function (listIndex, listItemIndex) {
+			var listId = favlist.lists[listIndex].id;
+			var listItemId = favlist.lists[listIndex].listItems[listItemIndex].id;
+
+			FavListItemsService.delete(
+				{ listId: listId, listItemId: listItemId },
+				function () {
+					// Remove from local $scope
+					favlist.lists[listIndex].listItems.splice(listItemIndex, 1);
+				}
+			);
+		}
 	});
